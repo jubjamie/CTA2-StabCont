1ff466fa2b2bba6dcae2fd2cdf70f1d3073f8a05
Jamie Cooke <jamie@iamjamiecooke.com>
2018-03-22 18:00:23 +0000

    Read cad Excel file into Pandas

--- scissorplot.py
+++ scissorplot.py
@@ -3,4 +3,11 @@ import numpy as np
 import pandas as pd
 
 # Excel Sheet Loading
-cad_file_path = ''
+cad_file_path = '../../../../2. TechInt/Project Space/CAD/CADParametersNewAero.xlsx'
+cad_file = pd.ExcelFile(cad_file_path)
+print(cad_file.sheet_names)
+
+cad_params = pd.read_excel(cad_file_path, sheet_name=0, index_col=0, skiprows=5, usecols="A:B")
+#print(cad_params.head(10))
+
+print(cad_params.shape)
