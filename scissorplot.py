2fa82f4433252f947b06e384caef285c4d8d2d34
Jamie Cooke <jamie@iamjamiecooke.com>
2018-03-22 18:36:37 +0000

    Open new branch with experimentation with openpyxl for creating parameter dictionary.

--- scissorplot.py
+++ scissorplot.py
@@ -1,13 +1,30 @@
 from matplotlib import pyplot
 import numpy as np
-import pandas as pd
+from openpyxl import load_workbook
 
 # Excel Sheet Loading
 cad_file_path = '../../../../2. TechInt/Project Space/CAD/CADParametersNewAero.xlsx'
-cad_file = pd.ExcelFile(cad_file_path)
-print(cad_file.sheet_names)
+cad_file = load_workbook(filename=cad_file_path, data_only=True)
+cad_params = cad_file['Sheet1']  # Load into Sheet1
 
-cad_params = pd.read_excel(cad_file_path, sheet_name=0, index_col=0, skiprows=5, usecols="A:B")
-#print(cad_params.head(10))
+# Init list holders
+param_name = []
+param_value = []
 
-print(cad_params.shape)
+# Loop helpers
+data_available = True
+data_row_start = 7  # Row where data starts
+
+# Loop through variable names and values, saving to list.
+while data_available is True:
+    #print(cad_params["A"+str(data_row_start)].value)
+    if cad_params["A"+str(data_row_start)].value is not None:
+        param_name.append(cad_params["A"+str(data_row_start)].value)
+        param_value.append(cad_params["B"+str(data_row_start)].value)
+        data_row_start = data_row_start+1
+    else:
+        break
+
+# Zip into a dictionary
+params = dict(zip(param_name, param_value))
+print(params)
