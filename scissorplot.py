8192722a195749791f3303345b3e265fa606d16f
Jamie Cooke <jamie@iamjamiecooke.com>
2018-03-25 18:40:46 +0100

    Add Nosewheel and Toke Off constraints initial attempts.

--- scissorplot.py
+++ scissorplot.py
@@ -17,7 +17,7 @@ data_row_start = 7  # Row where data starts
 
 # Loop through variable names and values, saving to list.
 while data_available is True:
-    #print(cad_params["A"+str(data_row_start)].value)
+    # print(cad_params["A"+str(data_row_start)].value)
     if cad_params["A"+str(data_row_start)].value is not None:
         param_name.append(cad_params["A"+str(data_row_start)].value)
         param_value.append(cad_params["B"+str(data_row_start)].value)
@@ -27,4 +27,57 @@ while data_available is True:
 
 # Zip into a dictionary
 params = dict(zip(param_name, param_value))
-print(params)
+#print(params)
+
+
+# Utility Functions
+
+
+def q(v):
+    return 0.5 * 1.225 * (v**2)
+
+
+def qS(v):
+    return 0.5 * 1.225 * params['Sarea'] * (v**2)
+
+
+# Key Aircraft Params
+c_bar = np.mean([params['Ct'], params['Cr']])
+maxthrust = 44538  # N @ takoff
+vto = 62.1  # m/s
+cthrust = maxthrust / qS(vto)
+cm0 = -0.0663
+cl_to = 2.549
+clt = -1  # Tail CL
+mtow = 35590  # kg
+g = 9.81  # m/s/s
+h0 = params['WingChordStart']/c_bar
+
+
+def noseWheel():
+    nosepos = params['NoseGearPos']
+    mainpos = params['MainGearPos']
+    condition_point = (0.975*(mainpos-nosepos)) + nosepos
+    return condition_point/c_bar
+
+
+def mainGearReaction():
+    total_weight = mtow/qS(vto)
+    return 0
+
+
+def takeOffRotation(h):
+    cl_moment = cl_to * (h-h0)
+    ct_moment = cthrust * 0.25/c_bar  # Approx cg offset pos
+    main_gear_moment_distance = (params['MainGearPos']/c_bar) - h
+    weight_nondim = g*mtow/qS(vto)
+    tail_moment_arm = (params['TailRootRearPlane']/c_bar) - h
+
+    lhs_top = cm0 + cl_moment + ct_moment - (main_gear_moment_distance * (weight_nondim-cl_to))
+    lhs_bottom = (main_gear_moment_distance * cl_to) + (clt * tail_moment_arm)
+
+    return lhs_top/lhs_bottom
+
+
+
+print(takeOffRotation(5.6))
