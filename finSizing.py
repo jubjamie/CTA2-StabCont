fff352d7abd5d19634c7e00e67c48cc737c4b39d
Jamie Cooke <jamie@iamjamiecooke.com>
2018-04-06 15:32:31 +0100

    Add finSizing file and add Excel parameter code

---
+++ finSizing.py
@@ -0,0 +1,28 @@
+import numpy as np
+from openpyxl import load_workbook
+
+# Excel Sheet Loading
+cad_file_path = '../../../../2. TechInt/Project Space/CAD/CADParametersNewAero.xlsx'
+cad_file = load_workbook(filename=cad_file_path, data_only=True)
+cad_params = cad_file['Sheet1']  # Load into Sheet1
+
+# Init list holders
+param_name = []
+param_value = []
+
+# Loop helpers
+data_available = True
+data_row_start = 7  # Row where data starts
+
+# Loop through variable names and values, saving to list.
+while data_available is True:
+    # print(cad_params["A"+str(data_row_start)].value)
+    if cad_params["A"+str(data_row_start)].value is not None:
+        param_name.append(cad_params["A"+str(data_row_start)].value)
+        param_value.append(cad_params["B"+str(data_row_start)].value)
+        data_row_start = data_row_start+1
+    else:
+        break
+
+# Zip into a dictionary
+params = dict(zip(param_name, param_value))
